71741578725870c1d4971da556e3a8b04f1ba923
rev <anas.srikou@gmail.com>
2019-10-29 10:56:28 +0100

    TPs xamarin

---
+++ App2/App2/Exercice1.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Exercice1 : ContentPage
+    {
+        public Exercice1()
+        {
+            InitializeComponent();
+            switch (Device.RuntimePlatform)
+            {
+                case Device.Android:
+                    mylayout.Padding = new Thickness(20, 30, 20, 20);
+                    break;
+                case Device.UWP:
+                    mylayout.Padding = new Thickness(20, 40, 20, 20);
+                    break;
+            }
+        }
+    }
+}
